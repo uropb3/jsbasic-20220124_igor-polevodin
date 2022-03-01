2abd990702d7c8540da994015619142aa33456c3
Igor <igor.polevodin@gmail.com>
2022-03-01 23:14:40 +0300

    Решил задачи 1-4 (модуль 8)

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,46 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (product) {
+      let cartItem = this.cartItems.find(i => i.product.id === product.id);
+
+      if (cartItem) {
+        cartItem.count++;
+      } else {
+        cartItem = { 
+          product, 
+          count: 1 
+        };
+
+        this.cartItems.push(cartItem);
+      }
+  
+      this.onProductUpdate(cartItem);
+    }
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    const cartItem = this.cartItems.find(i => i.product.id === productId);
+    
+    cartItem.count += amount;
+
+    if (cartItem.count === 0) {
+      this.cartItems.splice(this.cartItems.indexOf(cartItem), 1);
+    }
+
+    this.onProductUpdate(cartItem);
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((totalCount, item) => totalCount + item.count, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((totalPrice, item) => totalPrice + item.count * item.product.price, 0);
   }
 
   onProductUpdate(cartItem) {
